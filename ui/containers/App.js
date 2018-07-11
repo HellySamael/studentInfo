ed080d3881a706ecdee7b9d181ad9b7b78907410
HeLLySamael <c.etancelin@gmail.com>
2018-07-11 23:48:00 +0200

    install semantic + redux, init project

---
+++ ui/containers/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default class App extends Component {
+    render () {
+        return (
+            <div>
+        <Button> test button </Button>
+         <p>This is my new react app</p>
+         </div>)
+    }
+}
