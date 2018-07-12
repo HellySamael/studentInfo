87da016695dc933b93fc24f3444adabd3a3b49c9
HeLLySamael <c.etancelin@gmail.com>
2018-07-12 06:52:57 +0200

    Init app finish, lets start!

--- ui/containers/App.js
+++ ui/containers/App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
+import InputPreview from '../components/InputPreview';
+import {connect} from 'react-redux';
 import { Button } from 'semantic-ui-react';
-
-export default class App extends Component {
+import { setMessage } from '../actions/message';
+import { Link } from 'react-router-dom';
+class App extends Component {
+    _onChange = (value) => {
+        this.props.dispatch(setMessage(value))
+    }
     render () {
+        const {message} = this.props.messageReducer;
         return (
-            <div>
-        <Button> test button </Button>
-         <p>This is my new react app</p>
-         </div>)
+         <div>   <InputPreview value = {message} 
+            onChange={this._onChange}/>
+            <Link to='/about'>
+        <Button>About</Button>
+</Link>
+        </div>
+    )
     }
 }
+
+export default connect(state => state)(App);
